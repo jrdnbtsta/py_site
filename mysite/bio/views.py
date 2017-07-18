750f31035f006d84a17746e5d5ddd37f078e9ba4
Jordan Bautista <jordanbau@gmail.com>
2017-07-18 15:21:36 -0700

    working simple contact page

--- mysite/bio/views.py
+++ mysite/bio/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def index(request):
 	return render(request, 'bio/home.html')
+
+def contact(request):
+	return render(request, 'bio/basic.html', {'content' : ['If you would like to contact me', 'jordanbau@gmail.com']})
